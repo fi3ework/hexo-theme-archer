d92944ddce792bd0f4e4189cba676cf7b20369a2
fi3ework <fi3ework@gmail.com>
2017-06-20 23:49:56 +0800

    reconsitution header

--- source/script/header.js
+++ source/script/header.js
@@ -1,30 +1,29 @@
 (function () {
     let titleEle = document.getElementsByClassName("post-title")[0];
-    let titleHeight = getAbsPoint(titleEle).y;
-    let blogTitles = document.getElementsByClassName("blog-titles")[0];
-    let postTitle = document.getElementsByClassName("post-banner")[0];
+    let titleHeight = getAbsPosition(titleEle).y;
+    let toggleBanner = document.getElementsByClassName("site-post-banner")[0];
     let state = 0;
-
+    console.log(titleHeight);
     function toggleHeder() {
+        console.log(document.body.scrollTop);
         if (document.body.scrollTop > titleHeight) {
             if (!state) {
-                blogTitles.classList.add("blog-titles-hidden");
-                postTitle.classList.add("post-banner-show");
+                console.log(111);
+                toggleBanner.classList.add("post-banner-show");
+                toggleBanner.classList.remove("blog-banner-show");
                 state = 1;
-
             }
         } else {
             if (state) {
-                blogTitles.classList.remove("blog-titles-hidden");
-                postTitle.classList.remove("post-banner-show");
+                toggleBanner.classList.add("blog-banner-show");
+                toggleBanner.classList.remove("post-banner-show");
                 state = 0;
             }
         }
 
     }
 
-    function getAbsPoint(e) {
-        //再封装个函数吧。传进来的e可以是字符串类型（即id）,也可以是htmlElement对象。觉得getEL是个累赘的话，就把它删除掉。
+    function getAbsPosition(e) {
         var x = e.offsetLeft;
         var y = e.offsetTop;
         while (e = e.offsetParent) {
