f7cb270223a54d85a68401e84d8670add9888cf8
Wee <fi3ework@gmail.com>
2017-10-17 09:50:34 +0800

    fix #8

--- source-src/js/main.js
+++ source-src/js/main.js
@@ -4,7 +4,7 @@ import { initMobile } from './mobile';
 import { sidebarInit } from './sidebar';
 import { initTag } from './tag';
 
-console.info('hexo-theme-archer: v201701016');
+console.info('hexo-theme-archer: v201701017');
 init();
 toggleHeader();
 sidebarInit();
