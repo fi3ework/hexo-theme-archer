e6d1da254dc201b736487563b73b8bfaa27b7ba7
Wee <fi3ework@gmail.com>
2018-02-04 02:09:42 +0800

    refactor(tag): rewrite tag.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,25 @@
 import { init } from './init'
 import { scroll } from './scroll'
+import mySidebar from './initSidebar'
 import { initMobile } from './mobile'
-import sidebar from './sidebar'
 import InitSidebarLink from './tag'
 
 console.info('hexo-theme-archer: v20180109')
 init()
 scroll()
-let sidebarTags = new InitSidebarLink('tags', '.sidebar-tags-list', '.sidebar-tags-name')
-let sidebarCategories = new InitSidebarLink('categories', '.sidebar-categories-list', '.sidebar-categories-name')
+
+
+let metas = new InitSidebarLink()
+metas.addTab({
+  metaName: 'tags',
+  labelsContainer: '.sidebar-tags-name',
+  postsContainer: '.sidebar-tags-list'
+})
+
+metas.addTab({
+  metaName: 'categories',
+  labelsContainer: '.sidebar-categories-name',
+  postsContainer: '.sidebar-categories-list'
+})
+
 initMobile()
--- src/js/tag.js
+++ src/js/tag.js
@@ -1,32 +1,153 @@
 import archerUtil from './util'
-import sidebar from './sidebar'
+import sidebar from './initSidebar'
 import Emitter from 'eventemitter3'
 import { isArray } from 'util'
 
-class InitSidebarLink {
-  constructor(metaName, postList, labelsContainer) {
-    this._initIndexMap = this._initIndexMap.bind(this)
+class MetaInfo {
+  constructor(metaName, labelsContainer, postContainer) {
+    this.currLabelName = ''
+    this.isInited = false
+    this.postsArr = null
     this.metaName = metaName
-    this.currentQuery = ''
+    this.labelsContainer = $(labelsContainer)[0]
+    this.postContainer = $(postContainer)[0]
     this.indexMap = new Map()
-    this.postList = document.querySelector(postList)
-    this.labelsContainer = document.querySelector(labelsContainer)
-    this.initInfo() // lazy func
-    this.initIndexMap()
-    this.bindLabelClick()
-    this.bindOtherClick()
+    this._bindLabelClick()
   }
 
-  static postArr
-  static emitter = new Emitter()
-  initInfo() {
+  changeLabel(currLabelName) {
+    if (typeof currLabelName === 'string') {
+      this.currLabelName = currLabelName
+      this._changeList()
+      this._changeFocus()
+    }
+  }
+
+  _bindLabelClick() {
+    this.labelsContainer.addEventListener('click', (e) => {
+      let currLabelName = e.target.getAttribute(`data-${this.metaName}`)
+      this.changeLabel(currLabelName)
+    })
+  }
+
+  _changeFocus(label) {
+    let currFocus = this.labelsContainer.getElementsByClassName('sidebar-label-focus');
+    [...currFocus].forEach((item) => item.classList.remove('sidebar-label-focus'));
+    [...this.labelsContainer.children].forEach(item => {
+      if (item.getAttribute(`data-${this.metaName}`) === this.currLabelName) {
+        item.classList.add('sidebar-label-focus')
+      }
+    })
+  }
+
+  _changeList() {
+    let indexArr = this.indexMap.get(this.currLabelName)
+    try {
+      let corrArr = indexArr.map((index) => {
+        return this.postsArr[index]
+      })
+      this._createPostsDom(corrArr)
+    } catch (error) {
+      console.error('Please ensure set `tags: true` and `categories: true` of the hexo-content-json config')
+    }
+  }
+
+  _createPostsDom(corrArr) {
+    console.log(corrArr)
+    let frag = document.createDocumentFragment()
+    this.postContainer.innerHTML = ''
+    for (let i = 0; i < corrArr.length; i++) {
+      frag.appendChild(this._createPostDom(corrArr[i]))
+    }
+    this.postContainer.appendChild(frag)
+  }
+
+  _createPostDom(postInfo) {
+    let $tagItem = $('<li class="meta-post-item"><span class="meta-post-date">' + archerUtil.dateFormater(new Date(Date.parse(postInfo.date)), 'yyyy/MM/dd') + '</span></li>')
+    let $aItem = $('<a class="meta-post-title" href="' + jsInfo.root + postInfo.path + '">' + postInfo.title + '</a>')
+    $tagItem.append($aItem)
+    return $tagItem[0]
+  }
+
+  tryInit(postsArr) {
+    if (this.isInited ||
+            Object.prototype.toString.call(postsArr) === '[object Null]') {
+      return
+    }
+    for (let postIndex = 0; postIndex < postsArr.length; postIndex++) {
+      let currPostLabels = postsArr[postIndex][this.metaName]
+      // if there is any post has a tag
+      if (currPostLabels && currPostLabels.length) {
+        currPostLabels.forEach((tag) => {
+          if (this.indexMap.has(tag.slug)) {
+            this.indexMap.get(tag.slug).push(postIndex)
+          } else {
+            this.indexMap.set(tag.slug, [postIndex])
+          }
+        })
+      }
+    }
+
+    if (!this.indexMap.size) {
+      document.getElementsByClassName(`sidebar-${this.metaName}-empty`)[0].classList.add(`sidebar-${this.metaName}-empty-active`)
+    }
+
+    this.postsArr = postsArr
+    this.isInited = true
+  }
+}
+
+class SidebarMeta {
+  constructor(tabCount) {
+    this.tabCount = 0
+    this.emitter = new Emitter()
+    this.postsArr = null
+    this.metas = []
+    this._initMap = this._initMap.bind(this)
+    this.dataMaps = new Map()
+    this._subscribe()
+    this._fetchInfo()
+    this._bindOtherClick()
+  }
+
+  // add a new tab and updata all metas
+  addTab(para) {
+    this.tabCount++
+    let newMeta = new MetaInfo(para.metaName, para.labelsContainer, para.postsContainer)
+    newMeta.tryInit(this.postsArr)
+    this.metas.push(newMeta)
+  }
+
+  // update every MetaInfo
+  _tryInitMetas() {
+    for (let i = 0; i < this.metas.length; i++) {
+      this.metas[i].tryInit(this.postsArr)
+    }
+  }
+
+  // subscribe data onload
+  _subscribe() {
+    this.emitter.on('DATA_FETCHED_SUCCESS', this._initMap)
+  }
+
+  // init maps
+  _initMap() {
+    if (!this.postsArr.length) {
+      return
+    }
+    this._tryInitMetas()
+  }
+
+  // fetch content.json
+  _fetchInfo() {
     // jsInfo is from js-info.ejs
     let contentURL = jsInfo.root + 'content.json?t=' + (Number(new Date()))
     let xhr = new XMLHttpRequest()
     xhr.responseType = ''
     xhr.open('get', contentURL, true)
     let $loadFailed = $('.tag-load-fail')
-    xhr.onload = function () {
+    let that = this
+    xhr.onload = function() {
       if (this.status === 200 || this.status === 304) {
         $loadFailed.remove()
         // defensive programming if content.json formart is not correct
@@ -36,41 +157,17 @@ class InitSidebarLink {
         contentJSON = JSON.parse(this.responseText)
         posts = isArray(contentJSON) ? contentJSON : contentJSON.posts
         if (posts && posts.length) {
-          InitSidebarLink.postArr = posts
-          InitSidebarLink.emitter.emit('json ininted')
+          that.postsArr = posts
+          that.emitter.emit('DATA_FETCHED_SUCCESS')
         }
       } else {
-        this.postList.remove()
+        this.$currPostsContainer.remove()
       }
     }
     xhr.send()
-    Object.getPrototypeOf(this).initInfo = () => (console.log('initInfo function is lazyed'))
   }
 
-  initIndexMap() {
-    InitSidebarLink.emitter.on('json ininted', this._initIndexMap)
-  }
-
-  updateList(corrArr) {
-    let frag = document.createDocumentFragment()
-    this.postList.innerHTML = ''
-    corrArr.forEach((item) => {
-      frag.appendChild(this._createPostDom(item)[0])
-    })
-    this.postList.appendChild(frag)
-  }
-
-  bindLabelClick() {
-    this.labelsContainer.addEventListener('click', (e) => {
-      let currLabelName = e.target.getAttribute(`data-${this.metaName}`)
-      if (typeof currLabelName === 'string') {
-        this.currLabelName = currLabelName
-        this.switchLabel(this.currLabelName)
-      }
-    })
-  }
-
-  bindOtherClick() {
+  _bindOtherClick() {
     document.body.addEventListener('click', (e) => {
       if (e.target.className === 'post-tag') {
         e.stopPropagation()
@@ -78,58 +175,12 @@ class InitSidebarLink {
         sidebar.switchTo(1)
         let currLabelName = e.target.getAttribute(`data-tags`)
         this.currLabelName = currLabelName
-        this.switchLabel(this.currLabelName)
-      }
-    })
-    Object.getPrototypeOf(this).bindOtherClick = () => (console.log('bindOtherClick function is lazyed'))
-  }
-
-
-  _switchFocus(label) {
-    let currFocus = this.labelsContainer.getElementsByClassName('sidebar-label-focus');
-    [...currFocus].forEach((item) => item.classList.remove('sidebar-label-focus'));
-    [...this.labelsContainer.children].forEach(item => {
-      if (item.getAttribute(`data-${this.metaName}`) === label) {
-        item.classList.add('sidebar-label-focus')
+        let tagMeta = this.metas[0]
+        tagMeta.changeLabel(this.currLabelName)
       }
     })
   }
 
-  switchLabel(label) {
-    this._switchFocus(label)
-    let indexArr = this.indexMap.get(label)
-    let corrArr = indexArr.map((index) => {
-      return InitSidebarLink.postArr[index]
-    })
-    this.updateList(corrArr)
-  }
-
-  _createPostDom(postInfo) {
-    let $tagItem = $('<li class="meta-post-item"><span class="meta-post-date">' + archerUtil.dateFormater(new Date(Date.parse(postInfo.date)), 'yyyy/MM/dd') + '</span></li>')
-    let $aItem = $('<a class="meta-post-title" href="' + jsInfo.root + postInfo.path + '">' + postInfo.title + '</a>')
-    $tagItem.append($aItem)
-    return $tagItem
-  }
-
-  _initIndexMap() {
-    let postArr = InitSidebarLink.postArr
-    for (let postIndex = 0; postIndex < postArr.length; postIndex++) {
-      let currPostTags = postArr[postIndex][this.metaName]
-      // if there is any post has a tag
-      if (currPostTags.length) {
-        currPostTags.forEach((tag) => {
-          if (this.indexMap.has(tag.slug)) {
-            this.indexMap.get(tag.slug).push(postIndex)
-          } else {
-            this.indexMap.set(tag.slug, [postIndex])
-          }
-        })
-      }
-    }
-    if (!this.indexMap.size) {
-      document.getElementsByClassName(`sidebar-${this.metaName}-empty`)[0].classList.add(`sidebar-${this.metaName}-empty-active`)
-    }
-  }
 }
 
-export default InitSidebarLink
+export default SidebarMeta
