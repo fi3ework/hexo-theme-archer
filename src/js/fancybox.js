6242294ec9760bbab1bf81f513d8b40ccf25f3f8
KuriyamaMirai <lovef@outlook.com>
2018-08-24 17:27:54 +0800

    Update fancybox.js
    fix fancybox data-caption

--- src/js/fancybox.js
+++ src/js/fancybox.js
@@ -1,7 +1,7 @@
 const fancyBoxInit = img => {
   let outer = img.outerHTML
   let imgSrc = /src="(.*)"/.exec(outer) && /src="(.*)"/.exec(outer)[1]
-  let imgAlt = /alt="(.*)"/.exec(outer) && /alt="(.*)"/.exec(outer)[1] || /title="(.*)"/.exec(outer) && /title="(.*)"/.exec(outer)[1]
+  let imgAlt = /alt="(.*)"/.exec(outer) && /alt="(.*)"/.exec(outer)[1] || /title="(.*)"/.exec(outer) && /title="(.*)"/.exec(outer)[1] || ''
   img.outerHTML =
     '<a class="fancy-link" href="' +
     imgSrc +
