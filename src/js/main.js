70f5adacee0ed86b414840243c7cd8d0681a1630
Wee <fi3ework@gmail.com>
2018-08-24 15:52:43 +0800

    build: update built file

--- src/js/main.js
+++ src/js/main.js
@@ -9,8 +9,8 @@ import fancybox from './fancybox'
 let logStyle =
   'color: #fff; background: #f75357; padding: 1px; border-radius: 5px;'
 console.info('%c 🎯 hexo-theme-archer ⬇️ ', logStyle)
-console.info('%c 🏷 Version: 1.5.3 ', logStyle)
-console.info('%c 📅 Version date: 20180824', logStyle)
+console.info('%c 🏷 Version: 1.5.4 ', logStyle)
+console.info('%c 📅 Version date: 20180824 ', logStyle)
 console.info('%c 📦 https://github.com/fi3ework/hexo-theme-archer ', logStyle)
 
 // remove background placeholder
