b0a82fa747cc33c02151320b17eced32d42cee63
Lolipop <lolipop0703@qq.com>
2021-08-22 13:18:48 +0800

    perf: remove useless font-awsome package

---
+++ src/js/fontawsome.js
@@ -0,0 +1,4 @@
+// Only use 'fab' and 'fas' icons.
+import '@fortawesome/fontawesome-free/js/brands.min.js'
+import '@fortawesome/fontawesome-free/js/solid.min.js'
+import '@fortawesome/fontawesome-free/js/fontawesome.min.js'
