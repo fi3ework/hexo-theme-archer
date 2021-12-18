a74d9f05e652a9f2e3b2f0914b1a9340b31eaac2
Lolipop <lolipop0703@qq.com>
2021-12-18 16:33:11 +0800

    fix(Theme Mode): fix a bug in theme mode
    #325
    
    1. 修复了一个问题，这个问题导致系统在偏好为浅色模式下，无法正常根据用户偏好切换主题颜色模式
    2. 现在，浅色模式下也将拥有自深到浅的渐变，增加沉浸感

--- src/js/dark.js
+++ src/js/dark.js
@@ -1,18 +1,22 @@
-let currentThemeMode
 const $themeModeSwitchBtn = $('.header-theme-btn')
 
-// 获取当前主题颜色模式
+// 获取用户偏好的主题颜色模式
 const getPreferredThemeMode = function () {
-  const preferredThemeMode =
-    localStorage.preferredThemeMode !== undefined
-      ? localStorage.preferredThemeMode
-      : window.matchMedia('(prefers-color-scheme: dark)').matches
+  let preferredThemeMode = localStorage.preferredThemeMode
+  if (!preferredThemeMode) {
+    // 首次访问时，设置用户偏好主题颜色模式为系统偏好主题颜色模式
+    preferredThemeMode = window.matchMedia('(prefers-color-scheme: dark)')
+      .matches
       ? 'dark'
       : 'light'
-  return preferredThemeMode === 'dark' ? 'dark' : 'light'
+    localStorage.preferredThemeMode = preferredThemeMode
+  } else {
+    preferredThemeMode = preferredThemeMode === 'dark' ? 'dark' : 'light'
+  }
+  return preferredThemeMode
 }
 
-// 设置按钮可点击
+// 设置切换主题按钮可点击
 const setThemeModeSwitchBtnActive = (active) => {
   if (active) {
     $themeModeSwitchBtn.removeClass('header-theme-btn-disabled')
@@ -24,7 +28,11 @@ const setThemeModeSwitchBtnActive = (active) => {
 // 切换主题颜色模式
 const switchThemeMode = function (root) {
   setThemeModeSwitchBtnActive(false)
-  if ($(`LINK[href='${root}css/dark.css']`).length === 0) {
+  const $darkModeSourceLink = $(`LINK[href='${root}css/dark.css']`)
+  if ($darkModeSourceLink.length === 1) {
+    $darkModeSourceLink.remove()
+    localStorage.preferredThemeMode = 'light'
+  } else {
     $('<link>')
       .attr({
         rel: 'stylesheet',
@@ -33,26 +41,17 @@ const switchThemeMode = function (root) {
       })
       .appendTo('head')
     localStorage.preferredThemeMode = 'dark'
-  } else {
-    $(`LINK[href='${root}css/dark.css']`).remove()
-    localStorage.preferredThemeMode = 'light'
   }
   setThemeModeSwitchBtnActive(true)
-}
-
-// 获取当前的主题颜色模式
-const getCurrentThemeMode = (root) => {
-  return $(`LINK[href='${root}css/dark.css']`).length === 0 ? 'light' : 'dark'
 }
 
 // 初始化切换主题颜色模式功能
 const initThemeModeSwitchButton = function () {
   setThemeModeSwitchBtnActive(false)
 
-  // 默认情况下，当前主题颜色为系统偏好的颜色，
-  // 而当前主题颜色模式与用户偏好的主题颜色模式不同时，
-  // 切换主题颜色模式
-  if (getCurrentThemeMode(window.siteMeta.root) !== getPreferredThemeMode()) {
+  // 默认情况下，加载暗色主题
+  // 若用户偏好浅色主题，则切换主题颜色模式
+  if (getPreferredThemeMode() === 'light') {
     switchThemeMode(window.siteMeta.root)
   }
 
